78bb54f25e68d0ee6a3d8f00803df14a2fd00f9e
raheemadamboev <vividricemaryandmax@gmail.com>
2022-07-12 18:08:08 +0500

    setup material3 theme
    Took 22 minutes

--- app/src/main/java/xyz/teamgravity/weatherforecast/presentation/theme/Color.kt
+++ app/src/main/java/xyz/teamgravity/weatherforecast/presentation/theme/Color.kt
@@ -2,10 +2,47 @@ package xyz.teamgravity.weatherforecast.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val White = Color(0xFFFFFFFF)
+val White700 = Color(0xFFBBBBBB)
+val DarkGray = Color(0xFF121212)
+val DarkGray200 = Color(0xFF1A1A1A)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue10 = Color(0xFF0C1A27)
+val Blue20 = Color(0xFF17334F)
+val Blue30 = Color(0xFF234D76)
+val Blue40 = Color(0xFF2F669D)
+val Blue80 = Color(0xFFB0CCE8)
+val Blue90 = Color(0xFFD8E6F3)
+
+val DeepBlue10 = Color(0xFF0A1A29)
+val DeepBlue20 = Color(0xFF143352)
+val DeepBlue30 = Color(0xFF1F4D7A)
+val DeepBlue40 = Color(0xFF2966A3)
+val DeepBlue80 = Color(0xFFADCCEB)
+val DeepBlue90 = Color(0xFFD6E6F5)
+
+val Violet10 = Color(0xFF330033)
+val Violet20 = Color(0xFF660066)
+val Violet30 = Color(0xFF990099)
+val Violet40 = Color(0xFFCC00CC)
+val Violet80 = Color(0xFFFF99FF)
+val Violet90 = Color(0xFFFFCCFF)
+
+val Red10 = Color(0xFF410001)
+val Red20 = Color(0xFF680003)
+val Red30 = Color(0xFF930006)
+val Red40 = Color(0xFFBA1B1B)
+val Red80 = Color(0xFFFFB4A9)
+val Red90 = Color(0xFFFFDAD4)
+
+val Grey10 = Color(0xFF191C1D)
+val Grey20 = Color(0xFF2D3132)
+val Grey90 = Color(0xFFE0E3E3)
+val Grey95 = Color(0xFFEFF1F1)
+val Grey99 = Color(0xFFFBFDFD)
+
+val BlueGrey30 = Color(0xFF3C4D5D)
+val BlueGrey50 = Color(0xFF63809C)
+val BlueGrey60 = Color(0xFF8399AF)
+val BlueGrey80 = Color(0xFFC1CCD7)
+val BlueGrey90 = Color(0xFFE0E6EB)
--- app/src/main/java/xyz/teamgravity/weatherforecast/presentation/theme/Theme.kt
+++ app/src/main/java/xyz/teamgravity/weatherforecast/presentation/theme/Theme.kt
@@ -3,11 +3,7 @@ package xyz.teamgravity.weatherforecast.presentation.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
@@ -16,33 +12,68 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BlueGrey30,
+    onPrimary = White,
+    primaryContainer = Blue30,
+    onPrimaryContainer = White700,
+    inversePrimary = Blue40,
+    secondary = DeepBlue80,
+    onSecondary = DeepBlue20,
+    secondaryContainer = DeepBlue30,
+    onSecondaryContainer = DeepBlue90,
+    tertiary = Violet80,
+    onTertiary = Violet20,
+    tertiaryContainer = Violet30,
+    onTertiaryContainer = Violet90,
+    error = Red80,
+    onError = Red20,
+    errorContainer = Red30,
+    onErrorContainer = Red90,
+    background = DarkGray,
+    onBackground = White700,
+    surface = DarkGray200,
+    onSurface = White700,
+    inverseSurface = Grey20,
+    inverseOnSurface = Grey95,
+    surfaceVariant = DarkGray200,
+    onSurfaceVariant = White700,
+    outline = BlueGrey80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Blue40,
+    onPrimary = White,
+    primaryContainer = Blue90,
+    onPrimaryContainer = Blue10,
+    inversePrimary = Blue80,
+    secondary = DeepBlue40,
+    onSecondary = White,
+    secondaryContainer = DeepBlue90,
+    onSecondaryContainer = DeepBlue10,
+    tertiary = Violet40,
+    onTertiary = White,
+    tertiaryContainer = Violet90,
+    onTertiaryContainer = Violet10,
+    error = Red40,
+    onError = White,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+    background = Grey99,
+    onBackground = Grey10,
+    surface = BlueGrey90,
+    onSurface = BlueGrey30,
+    inverseSurface = Grey20,
+    inverseOnSurface = Grey95,
+    surfaceVariant = BlueGrey90,
+    onSurfaceVariant = BlueGrey30,
+    outline = BlueGrey50
 )
 
 @Composable
 fun WeatherForecastTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -62,7 +93,6 @@ fun WeatherForecastTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
         content = content
     )
 }

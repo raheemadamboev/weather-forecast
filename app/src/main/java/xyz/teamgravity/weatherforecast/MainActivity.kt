60740af0f6284e7eed6df0d8471bc44a8ea969a6
raheemadamboev <vividricemaryandmax@gmail.com>
2022-07-12 17:24:57 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/weatherforecast/MainActivity.kt
@@ -0,0 +1,26 @@
+package xyz.teamgravity.weatherforecast
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import xyz.teamgravity.weatherforecast.ui.theme.WeatherForecastTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherForecastTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}

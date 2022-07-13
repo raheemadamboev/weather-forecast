8b78b7543031904f42f569ff890c9b51df5f7d32
raheemadamboev <vividricemaryandmax@gmail.com>
2022-07-13 13:16:10 +0500

    setup weather repository
    Took 5 minutes

--- app/src/main/java/xyz/teamgravity/weatherforecast/domain/repository/WeatherRepository.kt
+++ app/src/main/java/xyz/teamgravity/weatherforecast/domain/repository/WeatherRepository.kt
@@ -2,13 +2,14 @@ package xyz.teamgravity.weatherforecast.domain.repository
 
 import kotlinx.coroutines.flow.Flow
 import xyz.teamgravity.weatherforecast.core.util.Resource
+import xyz.teamgravity.weatherforecast.domain.model.WeatherDataModel
 import xyz.teamgravity.weatherforecast.domain.model.WeatherModel
 
 interface WeatherRepository {
 
     ///////////////////////////////////////////////////////////////////////////
-    // Get
+    // GET
     ///////////////////////////////////////////////////////////////////////////
 
-    suspend fun getWeather(latitude: Double, longitude: Double): Flow<Resource<List<List<WeatherModel>>>>
+    suspend fun getWeather(latitude: Double, longitude: Double): Flow<Resource<WeatherModel>>
 }

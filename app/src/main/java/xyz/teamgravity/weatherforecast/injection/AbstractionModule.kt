2eba0623ce3a240d27201c8fc9bdc8bd6b78ef18
raheemadamboev <vividricemaryandmax@gmail.com>
2022-07-13 15:13:05 +0500

    inject dependencies
    Took 14 minutes

---
+++ app/src/main/java/xyz/teamgravity/weatherforecast/injection/AbstractionModule.kt
@@ -0,0 +1,31 @@
+package xyz.teamgravity.weatherforecast.injection
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Converter
+import retrofit2.converter.moshi.MoshiConverterFactory
+import xyz.teamgravity.weatherforecast.data.hardware.AndroidLocationTracker
+import xyz.teamgravity.weatherforecast.data.repository.WeatherRepositoryImp
+import xyz.teamgravity.weatherforecast.domain.hardware.LocationTracker
+import xyz.teamgravity.weatherforecast.domain.repository.WeatherRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AbstractionModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindConverterFactory(moshiConverterFactory: MoshiConverterFactory): Converter.Factory
+
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(weatherRepositoryImp: WeatherRepositoryImp): WeatherRepository
+
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(androidLocationTracker: AndroidLocationTracker): LocationTracker
+}
---
+++ app/src/main/java/xyz/teamgravity/weatherforecast/injection/ApplicationModule.kt
@@ -0,0 +1,70 @@
+package xyz.teamgravity.weatherforecast.injection
+
+import android.app.Application
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import xyz.teamgravity.weatherforecast.data.hardware.AndroidLocationTracker
+import xyz.teamgravity.weatherforecast.data.remote.api.WeatherApi
+import xyz.teamgravity.weatherforecast.data.remote.api.WeatherApiConst
+import xyz.teamgravity.weatherforecast.data.remote.datasource.WeatherDataSource
+import xyz.teamgravity.weatherforecast.data.repository.WeatherRepositoryImp
+import xyz.teamgravity.weatherforecast.domain.hardware.LocationTracker
+import xyz.teamgravity.weatherforecast.domain.repository.WeatherRepository
+import xyz.teamgravity.weatherforecast.domain.usecase.GetLocation
+import xyz.teamgravity.weatherforecast.domain.usecase.GetWeather
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshiConverterFactory(): MoshiConverterFactory = MoshiConverterFactory.create()
+
+    @Provides
+    @Singleton
+    fun provideWeatherApi(converterFactory: Converter.Factory): WeatherApi = Retrofit.Builder()
+        .baseUrl(WeatherApiConst.BASE_URL)
+        .addConverterFactory(converterFactory)
+        .build()
+        .create(WeatherApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideWeatherDataSource(weatherApi: WeatherApi): WeatherDataSource = WeatherDataSource(weatherApi)
+
+    @Provides
+    @Singleton
+    fun provideWeatherRepositoryImp(weatherDataSource: WeatherDataSource): WeatherRepositoryImp = WeatherRepositoryImp(weatherDataSource)
+
+    @Provides
+    @Singleton
+    fun provideFusedLocationProviderClient(application: Application): FusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(application)
+
+    @Provides
+    @Singleton
+    fun provideAndroidLocationTracker(
+        application: Application,
+        fusedLocationProviderClient: FusedLocationProviderClient,
+    ): AndroidLocationTracker = AndroidLocationTracker(
+        app = application,
+        client = fusedLocationProviderClient
+    )
+
+    @Provides
+    @Singleton
+    fun provideGetWeather(weatherRepository: WeatherRepository): GetWeather = GetWeather(weatherRepository)
+
+    @Provides
+    @Singleton
+    fun provideGetLocation(locationTracker: LocationTracker): GetLocation = GetLocation(locationTracker)
+}

a1cbee3dcc8dcb3d181930095ca5ead418825807
raheemadamboev <vividricemaryandmax@gmail.com>
2022-07-14 15:05:09 +0500

    build weather forecast
    Took 13 minutes

---
+++ app/src/main/java/xyz/teamgravity/weatherforecast/presentation/component/WeatherForecast.kt
@@ -0,0 +1,35 @@
+package xyz.teamgravity.weatherforecast.presentation.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import xyz.teamgravity.weatherforecast.R
+import xyz.teamgravity.weatherforecast.domain.model.WeatherDataModel
+
+@Composable
+fun WeatherForecast(
+    data: List<WeatherDataModel>
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.today),
+            style = MaterialTheme.typography.titleLarge
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyRow {
+            items(data) { weather ->
+                WeatherHourlyDisplay(weather = weather)
+            }
+        }
+    }
+}
--- app/src/main/java/xyz/teamgravity/weatherforecast/presentation/component/WeatherHourlyDisplay.kt
+++ app/src/main/java/xyz/teamgravity/weatherforecast/presentation/component/WeatherHourlyDisplay.kt
@@ -3,13 +3,17 @@ package xyz.teamgravity.weatherforecast.presentation.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import xyz.teamgravity.weatherforecast.R
 import xyz.teamgravity.weatherforecast.domain.model.WeatherDataModel
 import java.time.format.DateTimeFormatter
@@ -22,7 +26,10 @@ fun WeatherHourlyDisplay(
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .height(100.dp)
+            .padding(horizontal = 16.dp)
     ) {
         Text(text = time)
         Image(

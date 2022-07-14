2086fbe030f0b0ad477724da4623c4c18d8a5895
raheemadamboev <vividricemaryandmax@gmail.com>
2022-07-14 15:40:16 +0500

    inject dependencies to main
    Took 27 seconds

--- app/src/main/java/xyz/teamgravity/weatherforecast/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/weatherforecast/presentation/activity/Main.kt
@@ -7,8 +7,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+import xyz.teamgravity.weatherforecast.presentation.screen.WeatherScreen
 import xyz.teamgravity.weatherforecast.presentation.theme.WeatherForecastTheme
 
+@AndroidEntryPoint
 class Main : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +22,7 @@ class Main : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    WeatherScreen()
                 }
             }
         }

78944f61b19da29b6bcd29b40163be20027033d8
raheemadamboev <vividricemaryandmax@gmail.com>
2022-07-14 14:39:59 +0500

    build weather data display
    Took 24 seconds

---
+++ app/src/main/java/xyz/teamgravity/weatherforecast/presentation/component/WeatherDataDisplay.kt
@@ -0,0 +1,33 @@
+package xyz.teamgravity.weatherforecast.presentation.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WeatherDataDisplay(
+    value: Int,
+    unit: String,
+    @DrawableRes icon: Int,
+) {
+
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            painter = painterResource(id = icon),
+            contentDescription = null,
+            modifier = Modifier.size(25.dp)
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(text = derivedStateOf { "$value$unit" }.value)
+    }
+}

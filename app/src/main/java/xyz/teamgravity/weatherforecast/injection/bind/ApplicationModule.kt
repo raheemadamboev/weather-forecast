61970812dd6318d22de2f190d4b0b32e7ae3766f
raheemadamboev <vividricemaryandmax@gmail.com>
2022-07-16 22:05:31 +0500

    final refactoring
    Took 55 seconds

--- app/src/main/java/xyz/teamgravity/weatherforecast/injection/bind/ApplicationModule.kt
+++ app/src/main/java/xyz/teamgravity/weatherforecast/injection/bind/ApplicationModule.kt
@@ -14,7 +14,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class ApplicationAbstraction {
+abstract class ApplicationModule {
 
     @Binds
     @Singleton
